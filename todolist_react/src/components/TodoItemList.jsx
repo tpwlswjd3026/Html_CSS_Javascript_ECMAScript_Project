109390f6d879e52564edd7515c63eb14b70f0564
tpwlswjd3026 <tpwlswjd3026@gmial.com>
2026-09-16 15:52:54 +0900

    todolist

---
+++ todolist_react/src/components/TodoItemList.jsx
@@ -0,0 +1,29 @@
+import "./TodoItem.css";
+import { memo } from "react";
+ 
+const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
+    return (
+        <div className="todo-item" onClick={() => onToggle(id)}>
+            <div
+                className="remove"
+                onClick={(e) => {
+                    e.stopPropagation(); // onToggle 이 실행되지 않도록 함
+                    onRemove(id);
+                }}
+            >
+                &times;
+            </div>
+            <div className={`todo-text ${checked && "checked"}`}>
+                <div>{text}</div>
+            </div>
+            {checked && (<div className="check-mark">✓</div>)}
+        </div>
+    );
+};
+ 
+//export default TodoItem;
+// checked 가 바뀔 때만 다시 그린다
+export default memo(
+    TodoItem,
+    (prevProps, nextProps) => prevProps.checked === nextProps.checked
+);
